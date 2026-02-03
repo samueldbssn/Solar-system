f3b89793505bfc83d4cef3d26aa050aff8b1a500
sdubuiss <samuel.dubuisson@inria.fr>
2026-02-03 15:34:34 +0100

    :wrench: usage of Bear token for API

--- src/Api/ApiRequest.py
+++ src/Api/ApiRequest.py
@@ -1,16 +1,27 @@
 import json
 import requests
 
+import os
+from dotenv import load_dotenv
+
+
+
 class ApiRequest:
 
     def __init__(self, link):
         self.link = link
+        load_dotenv()
+        self.token = os.getenv("API_TOKEN")
+
 
     def request(self, params):
-        """Send a request to an api
+        """Send a request to an API using Bearer token authentication
 
-        :param params: the parameters sent to the api
-        :return: A hashmap with the results sent by the api
+        :param params: the parameters sent to the API
+        :return: A hashmap with the results sent by the API
         """
-        return requests.get(self.link, params=params).json()
-
+        headers = {
+            "Authorization": self.token
+        }
+        print(self.link, params, headers)
+        return requests.get(self.link, params=params, headers=headers).json()
